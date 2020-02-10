d614d0419a84879ca9bd8fdccb748b798dcdd1c6
Rodrigo Okuta <rodrigo.okuta@outlook.com>
2020-02-10 18:59:59 -0500

    Refactor Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import history from './history';
 import LandingLayout from '../layouts/Landing';
 import Login from '../pages/auth/Login';
 import Register from '../pages/auth/Register';
+import Chat from '../pages/chat';
+import Auth from '../util/Auth';
 
 const index = () => {
   return (
@@ -12,11 +14,24 @@ const index = () => {
       <Switch>
         <LandingLayout>
           <Switch>
-            <Route path="/Register">
+            <Route
+              path="/"
+              exact={true}
+              component={() => {
+                if (Auth.isUserAuthenticated()) {
+                  return <Redirect to="/Chat" />;
+                }
+                return <Redirect to="/Login" />;
+              }}
+            />
+            <Route path="/Login" exact={true}>
+              <Login />
+            </Route>
+            <Route path="/Register" exact={true}>
               <Register />
             </Route>
-            <Route path="/">
-              <Login />
+            <Route path="/Chat" exact={true}>
+              <Chat />
             </Route>
           </Switch>
         </LandingLayout>
