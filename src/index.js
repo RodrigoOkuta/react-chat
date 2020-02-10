c687b369e1a7f969dad557a714cce907159055c9
Rodrigo Okuta <rodrigo.okuta@outlook.com>
2020-02-10 12:07:44 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { toast } from 'react-toastify';
+
+toast.configure();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
